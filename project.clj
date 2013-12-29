64e067090e54c633e4de8d4d00771d384dd9ed01
Matt Gauger <matt.gauger@gmail.com>
2013-12-28 21:21:45 -0600

    Sketching.
    Inspired by http://www.ai.sri.com/~nysmith/papers/n65.pdf

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject renfield "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://github.com/mathias/renfield"
+  :description "...thou art my thrall and instrument."
+  :URL "https://github.com/mathias/renfield"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
--- src/renfield/core.clj
+++ src/renfield/core.clj
@@ -1,6 +1,33 @@
 (ns renfield.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defplan reduce-user-busyness-by-delegating-task
+  [task]
+  (where (and (> user-busyness user-busyness-threshold)
+              (delegatable-task?))
+         (create-candidate-goal :suggest-user-delegates task)))
+
+(defplan commence-next-step-of-shared-workflow
+  [task]
+  (where (and (shared-workflow-exists? task)
+              (task-completed? task)
+              (task-has-subtask? task)
+              (task-can-be-handled-by-renfield? task))
+         (create-candidate-goal :suggest-renfield-commence task)))
+
+(defplan notify-user-of-potential-syenrgy
+  [task]
+  (where (and (task-unfinished? task)
+              (task-has-related-task? task))
+         (create-candidate-goal :notify-potential-synergy task)))
+
+(defplan notify-user-of-change-to-schedule
+  [task]
+  (where (and (task-upcoming? task)
+              (task-changed-scheduling? task))
+         (create-candidate-goal :notify-user-of-change task)))
+
+(defplan renfield-can-notify-of-vacation
+  [email-event]
+  (where (and (user-on-vacation?)
+              (email-unread? email-event))
+         (create-candidate-goal :reply-with-vacation-reminder email-event)))
